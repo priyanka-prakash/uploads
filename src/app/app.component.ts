bb53a1404f5ca2aec15cb7c653f064e697e9a6bb
priyanka <priyanka.prakash@fingent.com>
2020-04-20 09:10:01 +0530

    image-upload-cropper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
+import {HttpClient, HttpEventType} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'image-uploader';
+  selectedFile:File  = null;
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+
+    fileChangeEvent(event: any): void {
+        console.log("event:",event);
+        this.imageChangedEvent = event;
+        this.selectedFile =  <File>event.target.files[0];
+    }
+    onUpload(){
+        const fd = new FormData();
+        fd.append('image', this.selectedFile, this.selectedFile.name);
+        this.http.post('https://us-central1-tutorial-e6ea7.cloudfunctions.net/fileUpload',fd, {
+            reportProgress:true,
+            observe:'events'
+        }).
+        subscribe( event => {
+            if(event.type === HttpEventType.UploadProgress){
+                console.log("progress:"+ Math.round( event.loaded / event.total * 100) + "%");
+            }else if(event.type == HttpEventType.Response){
+                console.log(event);
+            }
+           
+        });
+    }
+
+    constructor(private http:HttpClient){
+        // https://w3path.com/new-angular-8-file-upload-or-image-upload/
+    }
+
+
+
+
+
+    //kljsdlkALK;Jdfjklksdjflaks;djflaskjdfisldjflkds
+
+    imageCropped(event: ImageCroppedEvent) {
+        console.log("croppper ",event);
+        this.croppedImage = event.base64;
+    }
+    imageLoaded() {
+        // show cropper
+    }
+    cropperReady() {
+        // cropper ready
+    }
+    loadImageFailed() {
+        // show message
+    }
 }
